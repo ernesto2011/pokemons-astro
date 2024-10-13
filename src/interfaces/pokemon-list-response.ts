1366b9e11f760bc6b9289373c043950157adce0e
ernesto2011 <skillmx107@gmail.com>
2024-10-12 22:14:48 -0600

    pokemons list

---
+++ src/interfaces/pokemon-list-response.ts
@@ -0,0 +1,10 @@
+export interface PokemonListResponse {
+    count: number;
+    next: string;
+    previous: null;
+    results: Result[];
+}
+export interface Result {
+    name: string;
+    url: string;
+}
